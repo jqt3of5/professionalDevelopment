6bb6ec8ccdaa9989554b5841b523d97ec65607ce
jqt3of5 <jqt3of5@gmail.com>
2013-07-01 13:09:22 -0600

    re factored some more

--- jun2013/hwi/capture.c
+++ jun2013/hwi/capture.c
@@ -4,38 +4,78 @@
 #include <linux/i2c-dev.h>
 #include <sys/ioctl.h>
 #include <errno.h>
-
 #include "common.h"
-
 
 #define MAX_SAMPLES 10000
 
-static int fd;
 static char *fileName = "/dev/i2c-1";
 static int address = 0x68;
 
 int writeReg(int fd, char reg, char value);
 int readReg(int fd, char reg, char * value, int size);
 void swap(union sensor_raw * data);
+bool init(int & i2cFD);
+void readMeasurement(int i2cFD, union sensor_raw * data);
 
 char myError[200];
 
-bool init(FILE &* fd)
+struct MeasurementPair
 {
-    if ((fd = open(fileName, O_RDWR)) < 0) {
+	union sensor_raw measurement;
+	int timeStamp;
+};
+
+int main()
+{
+    int i2cFD;
+    //==== Configuration =================================
+    if (!init(i2cFD))
+    {
+    	printf(myError);
+    	return 1;
+    }
+
+    struct MeasurementPair bufx[MAX_SAMPLES] = {0};
+   
+// Spawn this into a new thread.......
+    int i = 0;
+    while(1)
+      {
+
+          readMeasurement(i2cFD, &bufx[i])
+
+          printf("time: %d | %d %d %d %d %d %d\n", bufx[i].timeStamp,
+		 bufx[i].measurement.reg.accx, bufx[i].measurement.reg.accy, bufx[i].measurement.reg.accz, 
+		 bufx[i].measurement.reg.gyrox, bufx[i].measurement.reg.gyroy, bufx[i].measurement.reg.gyroz);
+
+	  i += 1;
+	  i = i%MAX_SAMPLES;
+      }
+//=============================================================================================================
+
+}
+
+
+bool init(int & i2cFD)
+{
+	
+    if ((i2cFD = open(fileName, O_RDWR)) < 0) {
         sprintf(myError,"Failed to open the i2c bus error?%d\n ", errno);
         return false;
     }
 
-    if (ioctl(fd,I2C_SLAVE,address) < 0) {
+    if (ioctl(i2cFD,I2C_SLAVE,address) < 0) {
         sprintf(myError,"Failed to acquire bus access and/or talk to slave.\n");
 	return false;
     }
 
+    //Clear sleep bits
+    writeReg(i2cFD, 0x6B, 00);
+    writeReg(i2cFD, 0x6C, 00);
     return true;
 }
 
-void readMeasurement(FILE * i2cFD, union sensor_raw * data)
+void readMeasurement(int i2cFD, struct MeasurementPair * data)
 {
     char reg = 0x3B;
     if (write(i2cFD, &reg, 1) != 1)
@@ -43,47 +83,16 @@ void readMeasurement(FILE * i2cFD, union sensor_raw * data)
 	    fprintf(stderr, "Failed to write\n");
     	    return 0;
     	  }
-    	if (read(i2cFD, data, sizeof(union sensor_raw)) != sizeof(union sensor_raw))
+    	if (read(i2cFD, (*data).measurement, sizeof(union sensor_raw)) != sizeof(union sensor_raw))
     	  {
 	    fprintf(stderr, "Failed to read\n");
     	    return 0;
     	  }
-    	  swap(&bufx[i]);
+    	  
+    	  swap(&(*data).measurement);
+    	  (*data).timeStamp = time(0);
+    	  
 }
-
-int main()
-{
-    FILE *i2cFD;
-    //==== Configuration =================================
-    if (!init(i2cFD))
-    {
-    	printf(myError);
-    	return 1;
-    }
-    //Clear sleep bits
-    writeReg(i2cFD, 0x6B, 00);
-    writeReg(i2cFD, 0x6C, 00);
-    //other stuff?
-
-
-    union sensor_raw bufx[MAX_SAMPLES] = {0};
-   
-
-    int i = 0;
-    while(1)
-      {
-	
-          readMeasurement(i2cFD, &bufx[i])
-
-          printf("%d %d %d %d %d %d\n", 
-		 bufx[i].reg.accx, bufx[i].reg.accy, bufx[i].reg.accz, 
-		 bufx[i].reg.gyrox, bufx[i].reg.gyroy, bufx[i].reg.gyroz);
-
-	  i += 1;
-	  i = i%MAX_SAMPLES;
-      }
-}
-
 void swap(union sensor_raw * data)
 {
   int i = 0;
