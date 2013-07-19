b7f61b13d7ac328c3b55c9208ba4ea7c4ebc73d6
jqt3of5 <jqt3of5@gmail.com>
2013-07-19 12:58:36 -0600

    Update server.js

--- jun2013/stream/server.js
+++ jun2013/stream/server.js
@@ -1,6 +1,8 @@
 var net = require('net');
 var spawn = require('child_process').spawn;
 var readline = require('readline');
+var sys = require("sys"),
+    ws = require("./ws");
 
 
 var capture = spawn('../hwi/capture');
@@ -9,12 +11,35 @@ var rlInput = readline.createInterface({
     output: capture.stdin
 });
 
-var server = net.createServer(function(con) {
-    console.log("A client has connected");
 
-    rlInput.on('line', function(line) {
-	con.write(line);
+ws.createServer(function (websocket) {
+    websocket.addListener("connect", function (resource) { 
+      // emitted after handshake
+      sys.debug("connect: " + resource);
+
+      // server closes connection after 10s, will also get "close" event
+      setTimeout(websocket.end, 10 * 1000); 
+    }).addListener("data", function (data) { 
+      // handle incoming data
+      sys.debug(data);
+
+      // send data to client
+      rlInput.on('line', function(line) {
+	websocket.write(line);
+	});
+      //websocket.write(JSON.stringify(out));
+    }).addListener("close", function () { 
+      // emitted when server or client closes connection
+      sys.debug("close");
     });
+  }).listen(8080);
+
+//var server = net.createServer(function(con) {
+//    console.log("A client has connected");
+
+//    rlInput.on('line', function(line) {
+//	con.write(line);
+//    });
     // capture.stdout.on('data', function (data) {
     // 	con.write(data);
     // });
@@ -29,7 +54,7 @@ var server = net.createServer(function(con) {
     // 	process.stdout.write("end");
     // 	con.write("end");
     // });
-});
+//});
 
-server.listen(1234, function() {});
+//server.listen(1234, function() {});
 
